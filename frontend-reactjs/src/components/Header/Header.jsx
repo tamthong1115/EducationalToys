ed66ffe1e28799ee8b7084f12cb0286a8a63d690
Tam Thong <120663808+tamthong1115@users.noreply.github.com>
2024-10-11 08:11:59 +0700

    move login/register to seperate

--- frontend-reactjs/src/components/Header/Header.jsx
+++ frontend-reactjs/src/components/Header/Header.jsx
@@ -1,13 +1,36 @@
 import {
     SearchOutlined,
-    ShopOutlined,
+    // ShopOutlined,
     ShoppingCartOutlined,
 } from '@ant-design/icons'
 import { Button, Dropdown, Input, Menu } from 'antd'
 import { useState } from 'react'
+import Login from './Login'
+import Register from './Register'
+import ForgetPassword from './ForgetPassword'
 
 const Header = () => {
-    const [isOpen, setIsOpen] = useState(false)
+    // const [isOpen, setIsOpen] = useState(false)
+    const [buttonLogin, setButtonLogin] = useState(false)
+    const [buttonRegister, setButtonRegister] = useState(false)
+    const [buttonForgotPassword, setButtonForgotPassword] = useState(false)
+
+    const handleLogin = () => {
+        setButtonLogin(!buttonLogin)
+    }
+
+    const handleRegister = () => {
+        setButtonRegister(!buttonRegister)
+    }
+
+    const handleForgotPassword = () => {
+        setButtonForgotPassword(!buttonForgotPassword)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        // Add your submit logic here
+    }
 
     const categories = (
         <Menu>
@@ -27,54 +50,106 @@ const Header = () => {
     )
 
     return (
-        <header className="bg-white shadow-md">
-            <div className="mx-auto flex flex-col md:flex-row gap-3 justify-between items-center p-4">
-                <div className="text-2xl font-bold text-purple-700">
-                    Educational Toys
+        <>
+            <header className="bg-white shadow-md">
+                <div className="mx-auto flex flex-col md:flex-row gap-3 justify-between items-center p-4">
+                    <div className="text-2xl font-bold text-purple-700">
+                        Educational Toys
+                    </div>
+                    <Dropdown overlay={categories} trigger={['click']}>
+                        <Button className="bg-orange-500 text-white px-4 py-2 rounded">
+                            All categories
+                        </Button>
+                    </Dropdown>
+                    <Input
+                        placeholder="What are you looking for?"
+                        suffix={<SearchOutlined />}
+                        className="w-1/3"
+                    />
+                    <div className="flex flex-col md:flex-row gap-4 items-center space-x-4">
+                        <span className="text-gray-700">
+                            Get in touch: care@educationaltoys.co.uk
+                        </span>
+                        <div>
+                            <button
+                                className="mr-[10px] text-[18px] hover:italic hover:underline font-[400]"
+                                id="button-login"
+                                onClick={handleLogin}
+                            >
+                                Log In
+                            </button>
+                            <button
+                                className="text-[18px] hover:italic hover:underline bg-[#757FE2] p-[8px] rounded-[10px] font-[400]"
+                                onClick={handleRegister}
+                            >
+                                Register
+                            </button>
+                            <button className="hidden text-[18px] hover:italic hover:underline font-[400]">
+                                Log Out
+                            </button>
+                        </div>
+                        <ShoppingCartOutlined className="cursor-pointer" />{' '}
+                    </div>
                 </div>
-                <Dropdown overlay={categories} trigger={['click']}>
-                    <Button className="bg-orange-500 text-white px-4 py-2 rounded">
-                        All categories
-                    </Button>
-                </Dropdown>
-                <Input
-                    placeholder="What are you looking for?"
-                    suffix={<SearchOutlined />}
-                    className="w-1/3"
-                />
-                <div className="flex flex-col md:flex-row gap-4 items-center space-x-4">
-                    <span className="text-gray-700">
-                        Get in touch: care@educationaltoys.co.uk
-                    </span>
-                    <Button type="primary">Login</Button>
-                    <ShoppingCartOutlined className="cursor-pointer" />{' '}
-                </div>
-            </div>
 
-            {/* Navbar */}
-            <nav className="bg-gray-100  ">
-                <div className="mx-auto flex flex-col gap-10 md:flex-row items-center justify-center space-x-4 py-2">
-                    <a href="#" className="text-gray-700 hover:text-purple-700">
-                        Toys
-                    </a>
-                    <a href="#" className="text-gray-700 hover:text-purple-700">
-                        Tuff Trays
-                    </a>
-                    <a href="#" className="text-gray-700 hover:text-purple-700">
-                        Behavioural
-                    </a>
-                    <a href="#" className="text-gray-700 hover:text-purple-700">
-                        Sensory
-                    </a>
-                    <a href="#" className="text-gray-700 hover:text-purple-700">
-                        Outdoors
-                    </a>
-                    <a href="#" className="text-gray-700 hover:text-purple-700">
-                        Sand & Water
-                    </a>
-                </div>
-            </nav>
-        </header>
+                {/* Navbar */}
+                <nav className="bg-gray-100  ">
+                    <div className="mx-auto flex flex-col gap-10 md:flex-row items-center justify-center space-x-4 py-2">
+                        <a
+                            href="#"
+                            className="text-gray-700 hover:text-purple-700"
+                        >
+                            Toys
+                        </a>
+                        <a
+                            href="#"
+                            className="text-gray-700 hover:text-purple-700"
+                        >
+                            Tuff Trays
+                        </a>
+                        <a
+                            href="#"
+                            className="text-gray-700 hover:text-purple-700"
+                        >
+                            Behavioural
+                        </a>
+                        <a
+                            href="#"
+                            className="text-gray-700 hover:text-purple-700"
+                        >
+                            Sensory
+                        </a>
+                        <a
+                            href="#"
+                            className="text-gray-700 hover:text-purple-700"
+                        >
+                            Outdoors
+                        </a>
+                        <a
+                            href="#"
+                            className="text-gray-700 hover:text-purple-700"
+                        >
+                            Sand & Water
+                        </a>
+                    </div>
+                </nav>
+            </header>
+            {buttonLogin && (
+                <Login
+                    handleLogin={handleLogin}
+                    handleSubmit={handleSubmit}
+                    handleForgotPassword={handleForgotPassword}
+                    handleHaveNotAccount={handleRegister}
+                />
+            )}
+            {buttonRegister && <Register handleRegister={handleRegister} />}
+            {buttonForgotPassword && (
+                <ForgetPassword
+                    handleForgotPassword={handleForgotPassword}
+                    handleSubmit={handleSubmit}
+                />
+            )}
+        </>
     )
 }
 
