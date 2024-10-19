33a31e242a2a2e7f08a6731e36d7bc2cf8a17275
Lam Thanh Duy <thanhduythptthanhbinh@gmail.com>
2024-10-19 08:08:32 +0700

    update page about us

--- frontend-reactjs/src/pages/Home/Home.jsx
+++ frontend-reactjs/src/pages/Home/Home.jsx
@@ -1,10 +1,23 @@
+import Banner from "../../components/Banner/Banner"; 
+import Hero from "../../components/Hero/Hero";
 const Home = () => {
     return (
-      <div className="text-center">
-        <h1 className="text-3xl font-bold mb-4">Home Page</h1>
-        <p className="text-lg">Welcome to the home page of our application.</p>
-      </div>
+      <>
+        <div className="container mx-auto flex justify-center items-center">
+          <Banner/>
+        </div>
+        <div className="container mx-auto">
+          <Hero />
+        </div>
+        <div className="text-center">
+
+          <h1 className="text-3xl font-bold mb-4">Home Page</h1>
+          <p className="text-lg">Welcome to the home page of our application.</p>
+          
+        </div>
+      </>
+      
     );
   };
-  
+  // cai home nay lat phai xoa a
   export default Home;
