9c9f6391603f6cc93a0642a391c7683cf925abd7
Tam Thong <120663808+tamthong1115@users.noreply.github.com>
2024-10-29 00:55:31 +0700

    add patch category

--- backend/src/main/java/com/toyapp/backend/controller/admin/AdminCategoryController.java
+++ backend/src/main/java/com/toyapp/backend/controller/admin/AdminCategoryController.java
@@ -4,6 +4,7 @@ import com.toyapp.backend.dto.category.CategoryDTO;
 import com.toyapp.backend.dto.category.CreateCategoryDTO;
 import com.toyapp.backend.service.CategoryService;
 import com.toyapp.backend.service.CloudinaryService;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -39,11 +40,43 @@ public class AdminCategoryController extends BaseAdminController {
         Optional<CategoryDTO> updatedCategory = categoryService.updateCategory(id, categoryDTO);
         return updatedCategory.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
+    
+    @PatchMapping
+    public ResponseEntity<CategoryDTO> patchCategory(@RequestParam Long id, @RequestBody CategoryDTO categoryDTO) {
+        Optional<CategoryDTO> existingCategory = categoryService.getCategoryById(id);
+        if (existingCategory.isPresent()) {
+            CategoryDTO existing = getCategoryDTO(categoryDTO, existingCategory);
+            Optional<CategoryDTO> updatedCategory = categoryService.updateCategory(id, existing);
+            return updatedCategory.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @NotNull
+    private static CategoryDTO getCategoryDTO(CategoryDTO categoryDTO, Optional<CategoryDTO> existingCategory) {
+        if (existingCategory.isEmpty()) {
+            throw new IllegalArgumentException("Category not found");
+        }
+        CategoryDTO existing = existingCategory.get();
+        if (categoryDTO.getName() != null) {
+            existing.setName(categoryDTO.getName());
+        }
+        if (categoryDTO.getDescription() != null) {
+            existing.setDescription(categoryDTO.getDescription());
+        }
+        if (categoryDTO.getParentId() != null) {
+            existing.setParentId(categoryDTO.getParentId());
+        }
+        if (categoryDTO.getImage_url() != null) {
+            existing.setImage_url(categoryDTO.getImage_url());
+        }
+        return existing;
+    }
 
     @DeleteMapping
     public ResponseEntity<Void> deleteCategory(@RequestParam Long id) {
         categoryService.deleteCategory(id);
-        return ResponseEntity.noContent().build();
+        return ResponseEntity.ok().<Void>build();
     }
 
 
