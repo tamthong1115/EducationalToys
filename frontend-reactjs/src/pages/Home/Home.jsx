36fac1ba98b35bd1947f819e6172314de3b6a911
Lam Thanh Duy <thanhduythptthanhbinh@gmail.com>
2024-11-02 08:15:34 +0700

    Create list of toys

--- frontend-reactjs/src/pages/Home/Home.jsx
+++ frontend-reactjs/src/pages/Home/Home.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ToysList from '../../components/ToysDetail/ToysList';
 import Banner from "../../components/Banner/Banner"; 
 import Hero from "../../components/Hero/Hero";
+import { ListOfToy } from '../../components/ListOfToy/ListOfToy';
 const Home = () => {
     return (
       
@@ -14,6 +15,7 @@ const Home = () => {
         <h1 className="text-3xl font-bold mb-4">Home Page</h1>
         <p className="text-lg">Welcome to the home page of our application.</p>
         <ToysList/>
+        <ListOfToy/>
            
       </div>
       
